73e4fc8a4a41c3975f49fea6818eecf7d220b4fc
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-05-14 10:53:20 -0400

    fix(ui): confusing layout for options on mobile

--- src/app/(main)/options/index.tsx
+++ src/app/(main)/options/index.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx"
+import type React from "react"
 import type { ComponentPropsWithoutRef } from "react"
 import { Button } from "../../../components/button.tsx"
 import { FieldGroup, Fieldset } from "../../../components/fieldset.tsx"
@@ -22,16 +24,38 @@ export function QROptions(props: ComponentPropsWithoutRef<typeof Fieldset>) {
       </div>
       <Text>If in doubt, you can safely leave these options alone.</Text>
 
-      <FieldGroup className="sm:grid *:space-y-8 grid-cols-2 items-start sm:*:odd:pe-8 sm:*:even:ps-8 sm:*:even:border-l sm:*:even:border-zinc-950/10 sm:*:even:dark:border-white/10">
-        <div>
-          <BorderOption />
-          <ErrorCorrection />
-        </div>
-        <div>
-          <FormatOption />
-          <ScaleOption />
-        </div>
+      <FieldGroup className="grid gap-y-8 grid-cols-1 sm:grid-cols-2 items-start">
+        <ColumnWrapper>
+          <BorderOption className="order-1" />
+          <ErrorCorrection className="order-4" />
+        </ColumnWrapper>
+        <ColumnWrapper>
+          <FormatOption className="order-3" />
+          <ScaleOption className="order-2" />
+        </ColumnWrapper>
       </FieldGroup>
     </Fieldset>
+  )
+}
+
+/**
+ * Allows for more flexible "rows" in `grid` displays on desktop, while passing
+ * layout control to the parent on mobile.
+ */
+function ColumnWrapper({
+  className,
+  children,
+  ...rest
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div
+      {...rest}
+      className={clsx(
+        className,
+        "contents sm:block sm:space-y-8 sm:odd:pe-8 sm:even:ps-8 sm:even:border-l sm:even:border-zinc-950/10 sm:even:dark:border-white/10",
+      )}
+    >
+      {children}
+    </div>
   )
 }
